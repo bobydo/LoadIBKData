89e07ffd4e00f79927d5127275101e5d6446c670
Jerry Bao <jerrybao1998@outlook.com>
2021-05-12 22:01:49 -0600

    Added in multithreading, historical data works now

--- LoadIBKData/Service/HistoryDataService.cs
+++ LoadIBKData/Service/HistoryDataService.cs
@@ -16,7 +16,7 @@ namespace LoadIBKData.Service
         //private IBClient ibClient;
 
         public bool IsConnected { get; set; } = false;
-        //private EReaderMonitorSignal signal = new EReaderMonitorSignal();
+        private EReaderMonitorSignal signal = new EReaderMonitorSignal();
         public HistoryDataService(
             ILogger<HistoryDataService> logger
             ,IOptions<AppSetting> appSetting)
@@ -28,8 +28,8 @@ namespace LoadIBKData.Service
         public void GetData()
         {
             _logger.LogInformation("HistoricalData starts");
-            //CallFistOne();
-            CallAnother();
+            CallFistOne();
+            //CallAnother();
         }
 
         private void CallFistOne()
@@ -38,7 +38,7 @@ namespace LoadIBKData.Service
             // Ending date for the time series
             String strEndDate = "20210509 16:00:00";
             // Amount of time up to the end date
-            String strDuration = "1 M";
+            String strDuration = "1 W";
             // Bar size
             String strBarSize = "1 min";
             // Data type TRADES= OHLC Trades with volume
@@ -49,20 +49,23 @@ namespace LoadIBKData.Service
             int Port = Int32.Parse(_appSetting.Value.Port);
             int ClientId = Int32.Parse(_appSetting.Value.ClientId);
             ibClient.ClientSocket.eConnect(Host, Port, ClientId);
-            TimeSpan delay = new TimeSpan(0, 0, 5);
-            Thread.Sleep(delay);
+
+            EClientSocket clientSocket = ibClient.ClientSocket;
+
+            //Create a reader to consume messages from the TWS. The EReader will consume the incoming messages and put them in a queue
+            var reader = new EReader(clientSocket, signal);
+            reader.Start();
+            //Once the messages are in the queue, an additional thread can be created to fetch them
+            new Thread(() => { while (clientSocket.IsConnected()) { signal.waitForSignal(); reader.processMsgs(); } }) { IsBackground = true }.Start();
+
+
             // Create a new contract to specify the security we are searching for
             Contract contract = new Contract();
             contract.SecType = _appSetting.Value.SecType;
             contract.Symbol = _appSetting.Value.Symbol;
             contract.Exchange = _appSetting.Value.Exchange;
             contract.Currency = _appSetting.Value.Currency;
-            contract.LastTradeDateOrContractMonth = "";
             contract.PrimaryExch = _appSetting.Value.PrimaryExch;
-            contract.IncludeExpired = false;
-            contract.Strike = 0;
-            contract.Multiplier = "";
-            contract.LocalSymbol = "";
             // Create a new TagValue List object (for API version 9.71) 
             List<TagValue> historicalDataOptions = new List<TagValue>();
 
@@ -76,8 +79,8 @@ namespace LoadIBKData.Service
             // useRTH      - 1 = Use Real Time history
             // formatDate  - 3 = Date format YYYYMMDD
             // historicalDataOptions 
-            ibClient.ClientSocket.reqHistoricalData(requestId, contract, strEndDate, strDuration,
-                                                    strBarSize, strWhatToShow, 1, 3, false,
+            ibClient.ClientSocket.reqHistoricalData(requestId, contract, "", strDuration,
+                                                    strBarSize, strWhatToShow, 1, 1, true,
                                                     historicalDataOptions);
             // Pause to review data
             Console.ReadKey();
@@ -88,7 +91,7 @@ namespace LoadIBKData.Service
         private static void CallAnother()
         {
             EWrapperImpl ibClient = new EWrapperImpl();
-            ibClient.ClientSocket.eConnect("", 7496, 0);
+            ibClient.ClientSocket.eConnect("127.0.0.1", 7496, 0);
             TimeSpan delay = new TimeSpan(0, 0, 15);
             Thread.Sleep(delay);
 
