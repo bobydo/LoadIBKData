95099ec57d7347f913053a190ce0434fa3275fb6
Shenyi <baoshenyi@gmail.com>
2021-05-15 19:59:09 -0600

    Log from SQL debug

--- LoadIBKData/Data/APIDbContext.cs
+++ LoadIBKData/Data/APIDbContext.cs
@@ -1,7 +1,9 @@
 ﻿using LoadIBKData.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +14,18 @@ namespace LoadIBKData.Data
     {
         public APIDbContext(DbContextOptions<APIDbContext> options) : base(options)
         {
-
         }
 
         public DbSet<Price> Prices { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Log from SQL debug even for performance tuning
+            //optionsBuilder.LogTo(log => Debug.WriteLine(log));
+            optionsBuilder.LogTo(Console.WriteLine, new[] {DbLoggerCategory.Database.Command.Name},
+                LogLevel.Information)
+                .EnableSensitiveDataLogging();
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
         }
